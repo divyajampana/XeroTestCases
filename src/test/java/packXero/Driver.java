77c34dc355dfd145198020184f9048387a889de9
divyajampana <47876486+divyajampana@users.noreply.github.com>
2019-04-01 23:03:20 -0500

    Add files via upload

--- src/test/java/packXero/Driver.java
+++ src/test/java/packXero/Driver.java
@@ -6,23 +6,23 @@ public class Driver {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		AutomationScripts as = new AutomationScripts();
-		//as.testID01_A();
-		//as.testID01_B();
-		//as.testID01_C();
-		//as.testID01_D();
+		as.testID01_A();
+		as.testID01_B();
+		as.testID01_C();
+		as.testID01_D();
 		
-		//as.testID02_A();----------
-		//as.testID02_B();
-		//as.testID02_C();
-		//as.testID02_E();
+		//as.testID02_A();
+		as.testID02_B();
+		as.testID02_C();
+		as.testID02_E();
 		
 		as.testID03_A();
-		//as.testID04_A();
+		as.testID04_A();
 		//as.testID06_A();--------------
-		//as.testID08_A();---
-		//as.testID08_B();--------
-		//as.testID08_F();-----------
-		//as.testID010_A();
+		//as.testID08_A();
+		//as.testID08_B();
+		//as.testID08_F();
+		as.testID010_A();
 
 	}
 
